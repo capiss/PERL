63a8632f0fa47c7e89e04413c087fcb9cea17324
armando <armando2hdz@gmail.com>
2017-02-02 17:44:37 -0600

    Numeros pares

--- 01_AHernandez.pl
+++ 01_AHernandez.pl
@@ -5,14 +5,12 @@ use strict;
 
 my @numeros=(1..10);
 my @numerospares=();
-my $i=0;
 
 foreach(@numeros){
   if( $_%2 == 0){
-    $numerospares[$i]=$_;
+    push(@numerospares,$_);
     print $_,"\n";
-    $i=$i+1;
   }
 }
-
+print "\@numerospares: ";
 print @numerospares;
